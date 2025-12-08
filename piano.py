5d8a0a1691de56f568a4ec7d8dcba69994fccca1
mekhylab <bmekhyla@gmail.com>
2025-12-07 22:15:39 -0500

    final upload

--- piano.py
+++ piano.py
@@ -14,6 +14,28 @@ if width >= height:
 else:
     win=pygame.display.set_mode(SCREEN,pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
 
+clock = pygame.time.Clock()
+FPS = 30
+
+WHITE = (255, 255, 255)
+
 running= True
 while running:
-    
+    win.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    win.fill((255, 255, 255))
+
+    tile_width = WIDTH // 4
+    tile_height = HEIGHT // 4
+
+    for i in range(4):
+        for j in range(4):
+            rect = pygame.Rect(i * tile_width, j * tile_height, tile_width, tile_height)
+            pygame.draw.rect(win, (0, 0, 0), rect, 1)
+
+clock.tick(FPS)
+pygame.display.update()
+pygame.quit()
